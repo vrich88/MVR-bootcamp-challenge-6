5761e2cfd5f4c2a457d47aa68e9683889d3e5876
vrich88 <mvrichards@icloud.com>
2023-03-30 02:04:57 -0500

    beginning layout of html/css

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let apiKey = "d79e7c1302e1954e9746b6da76d3cba5";
+let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+let locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+let lat = latitude;
+let lon = longitude;
