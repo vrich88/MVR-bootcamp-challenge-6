15d79b55b3d5108384f2ac760cf72c3d4a41c448
vrich88 <mvrichards@icloud.com>
2023-04-04 16:30:52 -0500

    tweaks to layout and start over on script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 let apiKey = "d79e7c1302e1954e9746b6da76d3cba5";
-let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-let locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
-let lat = latitude;
-let lon = longitude;
+let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=" + apiKey;
+let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + geoLocation + "&appid=" + apiKey + "&units=imperial";
+let locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=" + apiKey;
+let latitude = data.coord.lat;
+let longitude = data.coord.lon;
+let temperature = data.main.temp;
+let windSpeed = data.wind.speed;
+let humidity = data.main.humidity;
+// event listener to trigger functions
+$(`#searchBtn`).on(`click`, function () {
+    getForecast();
+});
+
+// function to get forecast
+function getForecast() {
+
+};
