0e2ba73189f8cc4114a3b5fece6c555ed16cc8dc
vrich88 <mvrichards@icloud.com>
2023-04-07 08:30:00 -0500

    fetch api for current weaher functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,66 @@
+let searchForm = $(`#searchForm`);
+let newSearch = $(`#searchInput`).val();
+let searchBtn = $(`#searchBtn`);
+let recentList = $(`#recentList`);
+
+let recentCities = [];
+let lastCity = "";
+
 let apiKey = "d79e7c1302e1954e9746b6da76d3cba5";
-let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=" + apiKey;
-let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + geoLocation + "&appid=" + apiKey + "&units=imperial";
-let locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=" + apiKey;
-let latitude = data.coord.lat;
-let longitude = data.coord.lon;
-let temperature = data.main.temp;
-let windSpeed = data.wind.speed;
-let humidity = data.main.humidity;
-// event listener to trigger functions
-$(`#searchBtn`).on(`click`, function () {
-    getForecast();
-});
 
-// function to get forecast
-function getForecast() {
+// event listener for a new search
+searchBtn.on(`click`, function (event) {
+    event.preventDefault();
+    let citySearch = $(`#searchInput`).val();
+    getForecast(citySearch);
+    predictForecast(citySearch);
+    $(`#searchInput`).val("");
+}
+);
 
+// function to fetch data to get forecast
+function getForecast(citySearch) {
+    console.log(`getForecast on ` + citySearch)
+    if (citySearch !== "") {
+        let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+            + citySearch + "&units=imperial&appid=" + apiKey;
+        fetch(weatherUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json()
+                        .then(function (data) {
+                            $(`#currentTitle`).text(data.name);
+                            $(`#currentIcon`).append(`<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />`);
+                            $(`#currentDate`).text(dayjs(data.dt * 1000).format("MM/DD/YYYY"));
+                            $(`#currentTemperature`).text(data.main.temp.toFixed(0));
+                            $(`#currentWindSpeed`).text(data.wind.speed.toFixed(0));
+                            $(`#currentHumidity`).text(data.main.humidity.toFixed(0));
+                        })
+                } else {
+                    alert("There seems to be an issue finding your forecast. Please try entering the city again.")
+                }
+            })
+    } else {
+        alert("You need to search a city first to get your forecast.")
+    }
+};
+
+// function to fetch 5 day future forecast
+function predictForecast(citySearch) {
+    console.log(`predictForecast on ` + citySearch)
+    if (citySearch !== "") {
+        let futureUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
+            + citySearch + "&units=imperial&appid=" + apiKey;
+        fetch(futureUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json()
+                        .then(function (data) {
+                            // 
+                        })
+                } else {
+                    alert("There seems to be an issue finding the future forecast. Please try again later.")
+                }
+            })
+    }
 };
